dc7a66d94247a622c7136ea213327af00fc7334b
Keenest Callas <admin@elsikora.com>
2025-02-04 22:50:30 +0300

    refactor: update output directory from 'dist' to 'bin' in configuration files

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,20 @@
 import typescript from "@rollup/plugin-typescript";
 
 export default {
+	external: ["fs", "path", "openai", "chalk", "commander", "@anthropic-ai/sdk", "dotenv", "cli-progress", "@clack/prompts", "axios", "glob", "ignore", "@ai-ai/sdk", "fs/promises", "node:fs", "node:path", "node:os", "node:util", "rimraf", "node:child_process"],
 	input: "src/index.ts",
 	output: {
-		file: "dist/cli.js",
-		format: "esm",
-		sourcemap: true,
 		banner: "#!/usr/bin/env node",
 		exports: "auto",
+		file: "bin/cli.js",
+		format: "esm",
+		sourcemap: true,
 	},
-	external: ["fs", "path", "openai", "chalk", "commander", "@anthropic-ai/sdk", "dotenv", "cli-progress", "@clack/prompts", "axios", "glob", "ignore", "@ai-ai/sdk", "fs/promises", "node:fs", "node:path", "node:os", "node:util", "rimraf", "node:child_process"],
 	plugins: [
 		typescript({
-			tsconfig: "./tsconfig.json",
-			sourceMap: true,
 			declaration: false,
+			sourceMap: true,
+			tsconfig: "./tsconfig.json",
 		}),
 	],
 };
